e550d2ce5d01a93edbb90f4af05b1911af7213c3
fsheahan <fred@primeacademy.io>
2016-10-02 20:51:21 -0500

    corrected formatting

--- projectEulerQ1.cs
+++ projectEulerQ1.cs
@@ -1,6 +1,24 @@
-int result = 0;
-for (int i = 1; i < 1000; i++) {
-    if (((i % 3) == 0) || ((i % 5) == 0)) {
-        result += i;
+// Project Euler Problem #1
+// Find the sum of the multiples of 3 and 5 below 1000.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectEuler
+{
+    class EulerOne
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 1; i < 1000; i++)
+            {
+                if ((i % 3 == 0) || (i % 5 == 0))
+                    sum += i;
+            }
+            Console.WriteLine("The sum of the multiples of 3 and 5 below 1000 are: " + sum);
+        }
     }
 }
