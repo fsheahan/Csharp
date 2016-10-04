588cdbacb222e4fc9562fb103b5698c929978476
fsheahan <fred@primeacademy.io>
2016-10-03 21:13:08 -0500

    vs code build files

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -1,17 +1,12 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace HelloWorld
+namespace hwapp
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World");
-            Console.ReadLine();
-        }
-    }
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
 }
